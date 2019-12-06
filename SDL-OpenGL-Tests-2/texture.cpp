f9da8b82edb47679a565e7bf5d3866950ebb6930
Tobias Pflüger <tobiaspflueger@t-online.de>
2019-12-06 20:03:48 +0100

    Textures can be created from arrays

--- SDL-OpenGL-Tests-2/texture.cpp
+++ SDL-OpenGL-Tests-2/texture.cpp
@@ -13,8 +13,8 @@ textureName("tex"), fileName(fileName), type(type) {
     glGenTextures(1, &tex);
     glBindTexture(GL_TEXTURE_2D, tex);
 
-    unsigned char *image = SOIL_load_image(fileName.c_str(), &texWidth, &texHeight, 0, SOIL_LOAD_RGBA);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texWidth, texHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+    unsigned char *data = SOIL_load_image(fileName.c_str(), &texWidth, &texHeight, 0, SOIL_LOAD_RGBA);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texWidth, texHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
     
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
@@ -39,7 +39,38 @@ textureName("tex"), fileName(fileName), type(type) {
     
     glBindTexture(GL_TEXTURE_2D, 0);
     
-    SOIL_free_image_data(image);
+    SOIL_free_image_data(data);
+}
+
+Texture::Texture(unsigned char *data, unsigned int texWidth, unsigned int texHeight, TextureType type):
+textureName("tex"), type(type), texWidth(texWidth), texHeight(texHeight) {
+    glGenTextures(1, &tex);
+    glBindTexture(GL_TEXTURE_2D, tex);
+    
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texWidth, texHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+    
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    
+    switch (type) {
+        case TEXTURE_GENERATE_MIPMAP:
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_NEAREST);
+            
+            glGenerateMipmap(GL_TEXTURE_2D);
+            break;
+            
+        case TEXTURE_NO_MIP_MAP:
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+            
+        default:
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+            break;
+    }
+    
+    glBindTexture(GL_TEXTURE_2D, 0);
 }
 
 Texture::Texture() {
