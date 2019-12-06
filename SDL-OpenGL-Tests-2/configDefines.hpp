ab9bdbcd6824a1b96a3d24c99e3666104ae08ada
Tobias Pflüger <tobiaspflueger@t-online.de>
2019-12-06 20:00:28 +0100

    Added configuration defines

---
+++ SDL-OpenGL-Tests-2/configDefines.hpp
@@ -0,0 +1,14 @@
+//
+//  configDefines.hpp
+//  SDL-OpenGL-Tests-2
+//
+//  Created by Tobias Pflüger on 23.11.19.
+//  Copyright © 2019 Tobias Pflüger. All rights reserved.
+//
+
+#ifndef configDefines_h
+#define configDefines_h
+
+//#define PRINT_MAP_TIMES 1
+
+#endif /* configDefines_h */
