ad3716dc8b4e0a1167d6d8797171f0b34152c436
Tobias Pflüger <tobiaspflueger@t-online.de>
2019-12-06 20:01:35 +0100

    Added more lighting

--- SDL-OpenGL-Tests-2/resources/shaders/basic.fs
+++ SDL-OpenGL-Tests-2/resources/shaders/basic.fs
@@ -14,14 +14,14 @@ uniform int wireframe;
 void main() {
     if(wireframe == 1) {
         color = vec4(1.0f);
-        return;
+        return; 
     }
     
     vec3 normals = normalize(Normals);
     
-    vec3 lightDir = normalize(vec3(0.0f, 2.45f, 0.0f) - SpacePos);
+    vec3 lightDir = normalize(vec3(1.0f, 2.45f, 0.0f));
     float diff = max(dot(normals, lightDir), 0.0);
-    vec3 diffuse = diff * vec3(1.0f);
+    vec3 diffuse = diff * vec3(0.8f);
     
     float specularStrength = 0.5;
     vec3 viewDir = normalize(viewPos - SpacePos);
@@ -29,5 +29,5 @@ void main() {
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
     vec3 specular = specularStrength * spec * vec3(1.0f);
     
-    color = vec4(specular + diffuse + vec3(0.2f), 1.0f) * texture(tex, Uvs);
+    color = vec4(diffuse, 1.0f) * texture(tex, Uvs);
 }
